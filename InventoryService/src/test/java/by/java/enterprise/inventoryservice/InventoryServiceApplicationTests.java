6e94f0743e891d29abbee69ae59c1d9f31accb1d
The CNQRR <ap166884@gmail.com>
2026-05-14 21:52:20 +0700

    init

---
+++ InventoryService/src/test/java/by/java/enterprise/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.java.enterprise.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
